e3c22f465b993c13ee03f60e768b0ca7bc24fc27
Andrew <andrew@drews-air-2.lan>
2025-04-10 15:58:35 -0700

    Remove Disqus comments, keep only quotes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const quotes = [
-    { text: "The only way out is through.", id: "quote1" },
-    { text: "Do not fear mistakes. There are none.", id: "quote2" },
-    { text: "What you get by achieving your goals is not as important as what you become by achieving your goals.", id: "quote3" }
-  ];
+  "The only way out is through.",
+  "Do not fear mistakes. There are none.",
+  "What you get by achieving your goals is not as important as what you become by achieving your goals."
+];
+
   
   let current = 0;
   
   function showQuote(index) {
-    document.getElementById('quote-text').innerText = quotes[index].text;
-    loadDisqus(quotes[index].id);
+    document.getElementById('quote-text').innerText = quotes[index];
   }
+  
   
   function prevQuote() {
     current = (current - 1 + quotes.length) % quotes.length;
